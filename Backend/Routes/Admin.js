55bddd8f8918214e42ef454a53cd72b91d33f0ad
sanjeev <sanjeevgaund2002@gmail.com>
2023-04-30 12:13:57 +0530

    hi

--- Backend/Routes/Admin.js
+++ Backend/Routes/Admin.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const { adminRegister, adminLogin } = require('../controllers/Admin')
-const { addCompany, GetCompany, addDepartment } = require('../controllers/Company')
+const { addCompany, GetCompany, addDepartment, getDepartment } = require('../controllers/Company')
 const { isAuthenticatedAdmin } = require('../middlewares/Auth')
 const router = express.Router()
 
@@ -10,5 +10,6 @@ router.route("/login").post(adminLogin)
 router.route("/addCompany").post(addCompany)
 router.route("/getCompany").post( GetCompany)
 router.route("/addDept").post( addDepartment)
+router.route("/getDept/:id").post( getDepartment)
 
 module.exports = router
--- src/api/Admin.js
+++ src/api/Admin.js
@@ -1,16 +1,11 @@
 import axios from "axios"
 
+
 export const addCompany = async (values) => {
     try {
-        const data = await axios.post('http://localhost:5000/api/admin/addCompany',values)
-        return data
-    } catch (error) {
-        return error
-    }
-}
-export const getCompany = async (values) => {
-    try {
-        const data = await axios.post('http://localhost:5000/api/admin/getCompany',values)
+       
+        const data = await axios.post('http://localhost:5000/api/admin/addCompany', values)
+       
         return data
     } catch (error) {
         return error
