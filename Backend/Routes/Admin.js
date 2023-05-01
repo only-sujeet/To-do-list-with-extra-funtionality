c7466fc97cf001f4f573f8660fde60f395998919
Sujeet Maurya <109360865+only-sujeet@users.noreply.github.com>
2023-05-01 12:28:25 +0530

    sujeet

--- Backend/Routes/Admin.js
+++ Backend/Routes/Admin.js
@@ -4,7 +4,21 @@ const { addCompany, GetCompany,  addField, getField } = require('../controllers/
 const { addPeople } = require('../Controllers/People')
 const { isAuthenticatedAdmin } = require('../middlewares/Auth')
 const router = express.Router()
+const multer = require('multer')
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        if (file.fieldname === 'file') {
+            cb(null, 'Image')
+        } else {
+            cb(new Error('Invalid fieldname'), null);
+        }
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + '_' + file.originalname)
+    }
+})
 
+const upload = multer({ storage: storage })
 
 router.route("/register").post(adminRegister)
 router.route("/login").post(adminLogin)
@@ -12,5 +26,31 @@ router.route("/addCompany").post(addCompany)
 router.route("/getCompany").post( GetCompany)
 router.route("/addField").post( addField)
 router.route("/getField/:id").post( getField)
-router.route("/addPeople").post(addPeople)
+router.route("/addPeople").post( upload.single('file'), addPeople)
+// router.post('/addPeople',  upload.single('file'), async (req, res) => {
+//     try {
+//         // const {company,field,firstName,middleName,lastName,email,dob,age,mobileno,altmobileno,address1,address2,Image} = req.body
+//         const Image =(req.file) ? req.file.filename : null
+//         // const data = req.body.data
+//         // const object = JSON.parse(data)
+//         // const object = req.body
+//         // const {company} = res.body
+//         // const { company, field, firstName, middleName, lastName, email, dob, age, mobileno, altmobileno, address1, address2 } = req.body
+//         // const peo = new People({ company, field, firstName, middleName, lastName, email, dob, age, mobileno, altmobileno, address1, address2, Image })
+//         // await peo.save();
+//         // console.log("hello")
+//         res.status(200).json({
+//             success: true,
+//             message: "Successfully Added Profile",
+//             // peo:peo
+//              Image: Image,
+//             // object:company
+//         })
+//     } catch (error) {
+//         res.status(500).json({
+//             success: false,
+//             message: error.message,
+//         })
+//     }
+// })
 module.exports = router
--- Backend/controllers/People.js
+++ Backend/controllers/People.js
@@ -1,36 +1,23 @@
 
-const multer = require('multer');
 const People = require('../Models/Admin/People');
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        if (file.fieldname === 'file') {
-            cb(null, 'Image')
-        } else {
-            cb(new Error('Invalid fieldname'), null);
-        }
-    },
-    filename: function (req, file, cb) {
-        cb(null, Date.now() + '_' + file.originalname)
-    }
-})
 
-const upload = multer({ storage: storage })
-exports.addPeople = upload.single('file'), async (req, res) => {
+exports.addPeople =  async (req, res) => {
     try {
         // const {company,field,firstName,middleName,lastName,email,dob,age,mobileno,altmobileno,address1,address2,Image} = req.body
-        const Image = req.file['file'][0].filename
-        // const data = req.body.data
-        // const object = JSON.parse(data)
-        // const object = req.body
-        const { company, field, firstName, middleName, lastName, email, dob, age, mobileno, altmobileno, address1, address2 } = req.body
-        // const peo = new People({ company, field, firstName, middleName, lastName, email, dob, age, mobileno, altmobileno, address1, address2, Image })
-        // await peo.save();
+        const Image = (req.file) ? req.file.filename : null
+         const data = req.body.data
+         const object = JSON.parse(data)
+       
+        const { company, field, firstName, middleName, lastName, email, dob, age, mobileno, altmobileno, address1, address2 } = object
+         const peo = new People({ company, field, firstName, middleName, lastName, email, dob, age, mobileno, altmobileno, address1, address2, Image })
+         await peo.save();
+        // console.log("hello")
         res.status(200).json({
             success: true,
             message: "Successfully Added Profile",
             // peo:peo
-            Image: Image,
+             Image: Image,
             object:company
         })
     } catch (error) {
@@ -40,3 +27,29 @@ exports.addPeople = upload.single('file'), async (req, res) => {
         })
     }
 }
+// exports.addPeople =  async (req, res) => {
+//     try {
+//         // const {company,field,firstName,middleName,lastName,email,dob,age,mobileno,altmobileno,address1,address2,Image} = req.body
+//         // const Image = req.file['file'][0].filename
+//         // const data = req.body.data
+//         // const object = JSON.parse(data)
+//         // const object = req.body
+//         // const {company} = res.body
+//         // const { company, field, firstName, middleName, lastName, email, dob, age, mobileno, altmobileno, address1, address2 } = req.body
+//         // const peo = new People({ company, field, firstName, middleName, lastName, email, dob, age, mobileno, altmobileno, address1, address2, Image })
+//         // await peo.save();
+//         // console.log("hello")
+//         res.status(200).json({
+//             success: true,
+//             message: "Successfully Added Profile",
+//             // peo:peo
+//             // Image: Image,
+//             // object:company
+//         })
+//     } catch (error) {
+//         res.status(500).json({
+//             success: false,
+//             message: error.message,
+//         })
+//     }
+// }
