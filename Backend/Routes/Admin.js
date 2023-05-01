1ae61ffad2101fea13b44b78e3422bc3fce2fb4f
Sujeet Maurya <109360865+only-sujeet@users.noreply.github.com>
2023-05-01 12:41:10 +0530

    sujeet

--- Backend/Routes/Admin.js
+++ Backend/Routes/Admin.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const { adminRegister, adminLogin } = require('../controllers/Admin')
 const { addCompany, GetCompany,  addField, getField } = require('../controllers/Company')
-const { addPeople } = require('../Controllers/People')
+const { addPeople, getPeople } = require('../Controllers/People')
 const { isAuthenticatedAdmin } = require('../middlewares/Auth')
 const router = express.Router()
 const multer = require('multer')
+
+// for multer storage
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         if (file.fieldname === 'file') {
@@ -20,6 +22,7 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage })
 
+// Routes
 router.route("/register").post(adminRegister)
 router.route("/login").post(adminLogin)
 router.route("/addCompany").post(addCompany)
@@ -27,30 +30,6 @@ router.route("/getCompany").post( GetCompany)
 router.route("/addField").post( addField)
 router.route("/getField/:id").post( getField)
 router.route("/addPeople").post( upload.single('file'), addPeople)
-// router.post('/addPeople',  upload.single('file'), async (req, res) => {
-//     try {
-//         // const {company,field,firstName,middleName,lastName,email,dob,age,mobileno,altmobileno,address1,address2,Image} = req.body
-//         const Image =(req.file) ? req.file.filename : null
-//         // const data = req.body.data
-//         // const object = JSON.parse(data)
-//         // const object = req.body
-//         // const {company} = res.body
-//         // const { company, field, firstName, middleName, lastName, email, dob, age, mobileno, altmobileno, address1, address2 } = req.body
-//         // const peo = new People({ company, field, firstName, middleName, lastName, email, dob, age, mobileno, altmobileno, address1, address2, Image })
-//         // await peo.save();
-//         // console.log("hello")
-//         res.status(200).json({
-//             success: true,
-//             message: "Successfully Added Profile",
-//             // peo:peo
-//              Image: Image,
-//             // object:company
-//         })
-//     } catch (error) {
-//         res.status(500).json({
-//             success: false,
-//             message: error.message,
-//         })
-//     }
-// })
+router.route("/getPeople").get( getPeople)
+
 module.exports = router
--- Backend/controllers/People.js
+++ Backend/controllers/People.js
@@ -1,7 +1,7 @@
 
 const People = require('../Models/Admin/People');
 
-
+// For add People
 exports.addPeople =  async (req, res) => {
     try {
         // const {company,field,firstName,middleName,lastName,email,dob,age,mobileno,altmobileno,address1,address2,Image} = req.body
@@ -27,29 +27,43 @@ exports.addPeople =  async (req, res) => {
         })
     }
 }
-// exports.addPeople =  async (req, res) => {
-//     try {
-//         // const {company,field,firstName,middleName,lastName,email,dob,age,mobileno,altmobileno,address1,address2,Image} = req.body
-//         // const Image = req.file['file'][0].filename
-//         // const data = req.body.data
-//         // const object = JSON.parse(data)
-//         // const object = req.body
-//         // const {company} = res.body
-//         // const { company, field, firstName, middleName, lastName, email, dob, age, mobileno, altmobileno, address1, address2 } = req.body
-//         // const peo = new People({ company, field, firstName, middleName, lastName, email, dob, age, mobileno, altmobileno, address1, address2, Image })
-//         // await peo.save();
-//         // console.log("hello")
-//         res.status(200).json({
-//             success: true,
-//             message: "Successfully Added Profile",
-//             // peo:peo
-//             // Image: Image,
-//             // object:company
-//         })
-//     } catch (error) {
-//         res.status(500).json({
-//             success: false,
-//             message: error.message,
-//         })
-//     }
-// }
+
+// for get people
+exports.getPeople = async (req,res) => { 
+    try {
+        const request = await People.find()
+        if (!request) {
+            return res.status(404).json({success: false, message:"People Not found"})
+        }
+        else{
+            res.status(200).json(request)
+        }
+        
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message,
+        })
+    }
+ }
+
+//  for delete people
+exports.deletePeople = async (req,res) => { 
+    
+    try {
+        const request = await People.findById(req.params._id)
+        if (!request) {
+            return res.status(400).json({ message: "People Not Found" })
+        }
+        const reject =  await People.deleteOne({_id : req.params._id})
+        res.status(200).json({
+            success: true,
+            message: "Successfully Deleted People"
+        })  
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message,
+        })
+    }
+ } 
