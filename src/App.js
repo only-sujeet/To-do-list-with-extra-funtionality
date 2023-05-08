dea4b659902afcc470038b5295d53423a3c8e342
Sujeet Maurya <109360865+only-sujeet@users.noreply.github.com>
2023-05-08 09:53:24 +0530

    sujeet

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import AddProfile from './Components/Admin/AddProfile';
 import { ThemeProvider } from '@emotion/react';
 import theme from './Components/Custom/TypographyTheme';
 import BlockProfile from './Components/Admin/BlockProfile';
+import Task from './Components/Admin/Task';
 // import AdminTopbar from './Components/Global/AdminTopbar';
 
 function App() {
@@ -20,6 +21,7 @@ function App() {
       <Route path='/dashboard' element={<Dashboard/>}  />
       <Route path='/aprofile' element={<AddProfile/>}  />
       <Route path="/block_profile" element={<BlockProfile/>}  />
+      <Route path="/task" element={<Task/>}  />
     </Routes>
     </div>
     </ThemeProvider>
