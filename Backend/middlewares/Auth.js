9aabed51b7648390e56da177f7aa04c3e2658aa3
sanjeev <sanjeevgaund2002@gmail.com>
2023-07-21 18:01:38 +0530

    auth

--- Backend/middlewares/Auth.js
+++ Backend/middlewares/Auth.js
@@ -5,8 +5,9 @@ const People = require("../Models/Admin/People");
 exports.isAuthenticatedAdmin = async (req, res, next) => {
     try {
 
-        const { Token } = req.cookies;
+        const Token = req.header("Token")
         if (!Token) {
+
             return res
                 .status(401)
                 .json({ success: false, message: " Please login first.." })
@@ -15,6 +16,7 @@ exports.isAuthenticatedAdmin = async (req, res, next) => {
         const decode = jwt.verify(Token, process.env.SECRET_KEY)
         req.admin = await Admin.findById(decode._id)
         next();
+      
     } catch (error) {
         res.status(500).json({
             success: false,
